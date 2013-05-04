069e32544d3c67b0e1fea5ebff37227cae8465a0
Dio Perez Mavrogenis <dpm3g10@ecs.soton.ac.uk>
2013-05-04 14:32:27 +0100

    added a wikipedia exporer, for starters

---
+++ 121_intermediate_philosophy/parser.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+from sgmllib import SGMLParser
+import re
+
+class Parser(SGMLParser):
+	def __init__(self):
+		SGMLParser.__init__(self)
+		self.inRestr = False
+		self.currTitle = ''
+		self.Title = False
+		self.inMain = False 
+		self.inP = False 
+		self.frstLink = ''
+		self.first = True 
+		self.start_r = re.compile(r'[\(\[\{]')
+		self.end_r = re.compile(r'[\}\]\)]')
+
+	def handle_data(self, data):
+		if self.Title :
+			self.Title = False 
+			self.currTitle = data 
+		if self.inP :
+			if self.start_r.search(data) :
+				#print 'in parenthesis'
+				self.inRestr = True
+			elif self.end_r.search(data) :
+				#print 'closing parenthesis'
+				self.inRestr = False
+			#print 'reading data of ',SGMLParser.get_starttag_text(self),data
+
+	def start_span(self , attrs):
+		ad = self.dictify(attrs)
+		if len(attrs) == 1 and 'dir' in ad.keys()  and ad.get('dir') == 'auto':
+			self.Title = True
+	
+	def start_a(self, attrs):
+		ad = self.dictify(attrs)
+		if self.inP and not self.inRestr and self.first and self.inMain :
+			#print 'reading a:', ad.get('href')
+			if not ( 'class' in ad.keys() and ad.get('class') == 'mw-redirect' ):
+				self.first = False 
+				self.frstLink = ad.get('href')
+
+	def start_div(self, attrs):
+		ad = self.dictify(attrs)
+		if len(attrs) > 0 and 'id' in ad and ad.get('id') == 'mw-content-text':
+			self.inMain = True 
+
+	def result(self):
+		return self.currTitle, self.frstLink
+
+	def start_table (self, attrs):
+		self.inRestr = True
+	def end_table(self):
+		self.inRestr = False
+	def i_start(self, attrs):
+		self.inRestr = True 
+	def i_stop(self):
+		self.inRestr = False 
+	def start_p(self, attrs):
+		self.inP = True 
+	def stop_p(self):
+		self.inP = False
+	def start_sup(self, attrs):
+		self.inRestr = True 
+	def stop_sup(self):
+		self.inRestr = False
+	def start_sub(self, attrs):
+		self.inRestr = True 
+	def stop_sub(self):
+		self.inRestr = False
+	def dictify (self, attrs_lst):
+		return {k[0]:k[1] for k in attrs_lst}
---
+++ 121_intermediate_philosophy/wexplorer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys 
+import urllib2
+from parser import Parser
+from xml.dom.minidom import parseString
+from HTMLParser import HTMLParser
+
+if len(sys.argv) != 2 :
+	print 'usage : wexplore.py article-url'
+	sys.exit(-1)
+
+hist = [] 
+base = 'http://en.wikipedia.org'
+currT = '' 
+nextTarget = sys.argv[1]
+while (currT != 'Philosophy') :
+	hist.append(nextTarget)
+	print 'Looking at',nextTarget
+	request = urllib2.Request(nextTarget)
+	request.add_header('User-Agent','CLIClient/1.0')	
+	opener = urllib2.build_opener()
+	feeddata = opener.open(request).read()
+	opener.close()
+	par = Parser()
+	par.feed(feeddata)
+	currT, n =  par.result()
+	nextTarget = base+n
+
+print hist
